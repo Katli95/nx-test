5b1214fded2c8ab4aafa0ceb3b360c907b3cdfab
Ketill Antoníus Ágústsson <ketill@eno.is>
2024-12-09 10:21:17 +0100

    add import error description comments

--- packages/cart/src/lib/cart.ts
+++ packages/cart/src/lib/cart.ts
@@ -1,12 +1,18 @@
-// import { utilities } from '@here/utilities';
+// True Positive (Correctly Allows)
+import { utilities } from '@here/utilities';
+
+// False Positive (Incorrectly Allows)
 import { privatePrivate } from '@here/utilities/src/private/private';
+
+// True Negative (Correctly disallows)
+// import { privatePrivate } from '../../../utilities/src/private/private';
 
 export function cart(): string {
   const a = privatePrivate();
   console.log(a);
 
-  // const b = utilities();
-  // console.log(b);
+  const b = utilities();
+  console.log(b);
 
   return 'as';
 }
